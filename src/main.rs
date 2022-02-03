c4983ccac1186d48b4a23702e25ad1a750f34833
Ronald Chu <info@rchu.nl>
2022-02-03 16:55:23 +0000

    Minor changes to output and dependencies

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ fn main() {
         match Puzzle::from_file(&arg) {
             Ok(mut p) => match p.solve() {
                 Ok(p) => p.print_result(),
-                Err(e) => println!("  \x1b[32m Day {}\x1b[0m \x1b[31m{}\x1b[0m", p.day, e),
+                Err(e) => println!("  \x1b[32m Day {}\x1b[0m \x1b[31mError: {}\x1b[0m", p.day, e),
             },
-            Err(e) =>  println!("  \x1b[32m Day ??\x1b[0m {}: \x1b[31m{}\x1b[0m",arg, e),
+            Err(e) =>  println!("  \x1b[32m Day ??\x1b[0m {}: \x1b[31mError: {}\x1b[0m",arg, e),
         };
     }
 }
